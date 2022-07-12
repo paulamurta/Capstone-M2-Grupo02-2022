451759a7da1be9881702f18392e96ec65b0290cc
menezes-dev <menezes.icet@gmail.com>
2022-07-11 22:52:23 -0400

    feat: criação da listagem das tarefas

--- src/script/home.js
+++ src/script/home.js
@@ -1,3 +1,18 @@
-import { Header } from "../controllers/header.controllers.js";
+import Api from "../controllers/api.controllers.js";
+import FilterHabits from "../controllers/filter-habits.controllers.js";
+import TableHabit from "../controllers/table-habits.controllers.js";
+// import { Header } from "../controllers/header.controllers.js";
+// Header.headerInfoUser();
 
-Header.headerInfoUser();
+console.log(await Api.readAllHabits())
+FilterHabits.createFilters(await Api.readAllHabits());
+TableHabit.listHabit(await Api.readAllHabits());
+
+/* const newPost = {
+    habit_title: "Fazer Caminhada",
+    habit_description: "Exercícios físicos te ajudam a relaxar",
+    habit_category: "saude"
+}
+
+let testePost = await Api.createHabit(newPost); */
+// console.log(testePost);
