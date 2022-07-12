04d3827dae605003b423596dc9888f8385390bfd
Guilherme Wustro <gui.wustro@gmail.com>
2022-07-12 10:15:33 -0300

    feat: atualizado header dinâmico

--- src/script/home.js
+++ src/script/home.js
@@ -1,10 +1,14 @@
 import Api from "../controllers/api.controllers.js";
 import FilterHabits from "../controllers/filter-habits.controllers.js";
 import TableHabit from "../controllers/table-habits.controllers.js";
-// import { Header } from "../controllers/header.controllers.js";
-// Header.headerInfoUser();
+import Header from "../controllers/header.controllers.js";
+Header.headerInfoUser();
+//! Login para carregar a página, apagar depois
+Api.login({
+	email: "grupo2RafaelK@mail.com",
+	password: "8ad51ba84da177f2de190940dda6c9bc",
+});
 
-console.log(await Api.readAllHabits())
 FilterHabits.createFilters(await Api.readAllHabits());
 TableHabit.listHabit(await Api.readAllHabits());
 
