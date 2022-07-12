b56682005c5e0daeedca9ca1f2dac8c38321b146
Gabriel Martins <lima.gabriel@sou.ufac.br>
2022-07-11 23:04:57 -0500

    feat: login estilizado e localStorage

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -1,1 +1,41 @@
-class Login {}
+export default class Login {
+	static baseUrl = "https://habits-kenzie.herokuapp.com/api/userLogin";
+
+	static async userLogin(userData) {
+		await fetch(this.baseUrl, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(userData),
+		})
+			.then((response) => response.json())
+			.then((response) => {
+				console.log(response);
+
+				if (response.message === undefined) {
+					localStorage.setItem("@kenzie-habits:username", response.response.usr_name);
+					localStorage.setItem("@kenzie-habits:token", response.token);
+					window.location.href = "./src/views/home.html";
+				}
+			})
+			.catch((error) => console.log(error));
+	}
+
+	static formSubmit() {
+		const form = document.querySelector("#formLogin");
+
+		form.addEventListener("submit", async (event) => {
+			event.preventDefault();
+
+			let userData = new FormData(form);
+			userData = {
+				email: userData.get("email"),
+				password: userData.get("password"),
+			};
+
+			await this.userLogin(userData);
+		});
+	}
+
+}
