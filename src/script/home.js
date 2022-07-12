a9970b159eb247e0a78f5339f6ec18b17340077b
paulamurta <paulamurta@hotmail.com>
2022-07-12 14:06:10 -0400

    feat: Criação do Mobilw First

--- src/script/home.js
+++ src/script/home.js
@@ -4,15 +4,24 @@ import TableHabit from "../controllers/table-habits.controllers.js";
 // import { Header } from "../controllers/header.controllers.js";
 // Header.headerInfoUser();
 
+/* const objetoLogin = {
+    email: "grupo2RafaelK@mail.com",	
+    password: "8ad51ba84da177f2de190940dda6c9bc"
+    }
+
+Api.login(objetoLogin) 
+
 console.log(await Api.readAllHabits())
 FilterHabits.createFilters(await Api.readAllHabits());
 TableHabit.listHabit(await Api.readAllHabits());
 
-/* const newPost = {
+const newPost = {
     habit_title: "Fazer Caminhada",
     habit_description: "Exercícios físicos te ajudam a relaxar",
     habit_category: "saude"
 }
 
-let testePost = await Api.createHabit(newPost); */
+let testePost = await Api.createHabit(newPost); 
 // console.log(testePost);
+
+*/
