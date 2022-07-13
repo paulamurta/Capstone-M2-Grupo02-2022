97591304da71c012b848b62fb033f169475cca11
Gabriel Martins <lima.gabriel@sou.ufac.br>
2022-07-13 14:03:24 -0500

    fix: corrigido o redicionamento à home

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -1,33 +1,30 @@
 import Api from "./api.controllers.js";
 import ModalRequest from "../controllers/modal-requests.controllers.js";
 export default class Login {
-	static baseUrl = "https://habits-kenzie.herokuapp.com/api/userLogin";
-
 	static formSubmit() {
 		const form = document.querySelector("#formLogin");
-
 		form.addEventListener("submit", async (event) => {
 			event.preventDefault();
-
 			let userData = new FormData(form);
 			userData = {
 				email: userData.get("email"),
 				password: userData.get("password"),
 			};
 
-			await Api.login(userData)
-				.then((res) => {
-					if (res.token) {
-						ModalRequest.modalSucess("O usuário foi logado");
-						setTimeout(() => {
-							window.location.href = "./src/views/home.html";
-						}, 2000);
-					} else {
-						let message = res.message;
-						ModalRequest.modalError(message.charAt(message.length-1) === "." ? message.charAt(0).toUpperCase() + message.slice(1) : message.charAt(0).toUpperCase() + message.slice(1)  + ".");
-					}
-				});
+			const result = await Api.login(userData);
+			if (result.token) {
+				ModalRequest.modalSucess("O usuário foi logado");
+				setTimeout(() => {
+					window.location.href = "/src/views/home.html";
+				}, 1000);
+			} else {
+				let message = result.message;
+				ModalRequest.modalError(
+					message.charAt(message.length - 1) === "."
+						? message.charAt(0).toUpperCase() + message.slice(1)
+						: message.charAt(0).toUpperCase() + message.slice(1) + "."
+				);
+			}
 		});
 	}
-
 }
